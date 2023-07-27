f5fdd57ee6d86a0013720eaa4bf605a0afa5e0d8
norhs <norhsham02@gmail.com>
2023-07-27 05:18:46 +0300

    finish Partner

--- src/Components/Partner/Partner.jsx
+++ src/Components/Partner/Partner.jsx
@@ -6,29 +6,196 @@ import partner3 from "../../assets/imgs/partners3.png";
 const Partner = () => {
   return (
     <Stack className="section">
+      <Box className="mainTitle">
+        <Typography
+          sx={{
+            textAlign: "center",
+            fontSize: { xs: "25px", sm: "28px", md: "40px" },
+            lineHeight: { xs: 1.2, sm: "1.2" },
+            letterSpacing: { xs: "0px", sm: "0px" },
+            fontWeight: 700,
+            fontStyle: "normal",
+            fontFamily: "'Montserrat', sans-serif",
+          }}>
+          Our Partners
+        </Typography>
+      </Box>
       <Container>
-        <Box className="mainTitle">
-          <Typography>Our Partners</Typography>
-        </Box>
-        <Box className="partner1">
-          <img src={partner1} alt="" />
-          <Box className="text">
-            <Typography>Company Name</Typography>
-            <Typography>Company Description</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+          }}
+          className="parteners">
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              paddingTop: { xs: "25px", md: "40px" },
+              paddingLeft: "15px",
+              paddingRight: "15px",
+            }}
+            className="partner1">
+            <div
+              style={{
+                flex: " 0 0 50%",
+                maxWidth: "50%",
+              }}
+              className="iamge">
+              <img
+                style={{
+                  maxWidth: "100%",
+                }}
+                src={partner1}
+                alt=""
+              />
+            </div>
+
+            <Box
+              sx={{
+                flex: " 0 0 50%",
+                maxWidth: "50%",
+              }}
+              className="text">
+              <Typography
+                sx={{
+                  fontWeight: 700,
+                  fontStyle: "normal",
+                  fontFamily: "'Montserrat', sans-serif",
+                  color: "#232222",
+                  fontSize: "20px",
+                  lineHeight: 1.2,
+                  letterSpacing: "0px",
+                  paddingBottom: "20px",
+                }}>
+                Company Name
+              </Typography>
+              <Typography
+                sx={{
+                  fontWeight: 300,
+                  fontStyle: "normal",
+                  fontFamily: "'Roboto Slab', serif",
+                  color: "#6d6b6b",
+                  fontSize: "15px",
+                  lineHeight: 1.5,
+                  letterSpacing: "0px",
+                }}>
+                Company Description
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-        <Box className="partner2">
-          <img src={partner2} alt="" />
-          <Box className="text">
-            <Typography>Company Name</Typography>
-            <Typography>Company Description</Typography>
+
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              paddingTop: { xs: "25px", md: "40px" },
+              paddingLeft: "15px",
+              paddingRight: "15px",
+            }}
+            className="partner2">
+            <div
+              style={{
+                flex: " 0 0 50%",
+                maxWidth: "50%",
+              }}
+              className="iamge">
+              <img
+                style={{
+                  maxWidth: "100%",
+                }}
+                src={partner2}
+                alt=""
+              />
+            </div>{" "}
+            <Box
+              sx={{
+                flex: " 0 0 50%",
+                maxWidth: "50%",
+              }}
+              className="text">
+              <Typography
+                sx={{
+                  fontWeight: 700,
+                  fontStyle: "normal",
+                  fontFamily: "'Montserrat', sans-serif",
+                  color: "#232222",
+                  fontSize: "20px",
+                  lineHeight: 1.2,
+                  letterSpacing: "0px",
+                  paddingBottom: "20px",
+                }}>
+                Company Name
+              </Typography>
+              <Typography
+                sx={{
+                  fontWeight: 300,
+                  fontStyle: "normal",
+                  fontFamily: "'Roboto Slab', serif",
+                  color: "#6d6b6b",
+                  fontSize: "15px",
+                  lineHeight: 1.5,
+                  letterSpacing: "0px",
+                }}>
+                Company Description
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-        <Box className="partner3">
-          <img src={partner3} alt="" />
-          <Box className="text">
-            <Typography>Company Name</Typography>
-            <Typography>Company Description</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              paddingTop: { xs: "25px", md: "40px" },
+              paddingLeft: "15px",
+              paddingRight: "15px",
+            }}
+            className="partner3">
+            <div
+              style={{
+                flex: " 0 0 50%",
+                maxWidth: "50%",
+              }}
+              className="iamge">
+              <img
+                style={{
+                  maxWidth: "100%",
+                }}
+                src={partner3}
+                alt=""
+              />
+            </div>{" "}
+            <Box
+              sx={{
+                flex: " 0 0 50%",
+                maxWidth: "50%",
+              }}
+              className="text">
+              <Typography
+                sx={{
+                  fontWeight: 700,
+                  fontStyle: "normal",
+                  fontFamily: "'Montserrat', sans-serif",
+                  color: "#232222",
+                  fontSize: "20px",
+                  lineHeight: 1.2,
+                  letterSpacing: "0px",
+                  paddingBottom: "20px",
+                }}>
+                Company Name
+              </Typography>
+              <Typography
+                sx={{
+                  fontWeight: 300,
+                  fontStyle: "normal",
+                  fontFamily: "'Roboto Slab', serif",
+                  color: "#6d6b6b",
+                  fontSize: "15px",
+                  lineHeight: 1.5,
+                  letterSpacing: "0px",
+                }}>
+                Company Description
+              </Typography>
+            </Box>
           </Box>
         </Box>
       </Container>
